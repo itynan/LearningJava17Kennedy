c41df9f457060b2533c918e16d3df2077d10be53
Iliya Tynan <itynan89@gmail.com>
2023-03-26 19:58:56 -0400

    add Runtime class methods to VariousTypes class

--- src/concurrency/executor_service/VariousTypes.java
+++ src/concurrency/executor_service/VariousTypes.java
@@ -9,6 +9,16 @@ public class VariousTypes {
 
         int threadCount = Runtime.getRuntime().availableProcessors();
         System.out.println("Thread COUNT: " + threadCount);
+        long freeMemory = Runtime.getRuntime().freeMemory();
+        System.out.println("Free memory (bytes): " + freeMemory);
+        long totalMemory = Runtime.getRuntime().totalMemory();
+        System.out.println("total memory (bytes): " + totalMemory);
+        long maxMemory = Runtime.getRuntime().maxMemory();
+        System.out.println("max memory (bytes): " + maxMemory);
+
+        Runtime.Version runtimeVersion;
+        System.out.println(Runtime.version());
+
         ExecutorService es2 = Executors.newFixedThreadPool(threadCount);
 
         ExecutorService es3 = Executors.newSingleThreadExecutor();
